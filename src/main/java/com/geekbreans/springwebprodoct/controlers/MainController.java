2a86027a68db989095a9c4754b42ec1248592bb1
ZholudV <goldexzholud@gmail.com>
2022-04-21 17:46:59 +0500

    домашнее задание по web(вывод продуктов на страницу просмотра) добавлен комментарий в главном контроллере отправили на вторую ветку в уд репозитории создали форму для добавления продуктов

--- src/main/java/com/geekbreans/springwebprodoct/controlers/MainController.java
+++ src/main/java/com/geekbreans/springwebprodoct/controlers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,16 +21,30 @@ public class MainController {
 
 
     @GetMapping("/products")
-    public String showStudentsPage(Model model) {
+    public String showProductsPage(Model model) {
         model.addAttribute("products", productRepository.getAllProducts());
         return "products_page";
     }
 
     @GetMapping("/products/{id}")
-    public String showStudentPage(Model model, @PathVariable Long id) {
+    public String showProductsPage(Model model, @PathVariable Long id) {
         Product product = productRepository.findById(id);
         model.addAttribute("product", product);
         return "product_info_page";
+    }
+
+    @GetMapping("/products_show")
+    public String showPage() {
+
+        return "simple_form";
+    }
+    @GetMapping("/product_add")
+    public String addProduct(@RequestParam Long id, @RequestParam String name, @RequestParam Long price) {
+
+        Product product = new Product(id, name, price);
+        productRepository.add(product);
+
+        return "redirect:/products";
     }
 
 }
